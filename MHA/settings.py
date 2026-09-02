d09b5559fc73860592f301c1b8b7c37f2f7ea044
steve mavuela <stevemavuela@gmail.com>
2026-09-02 15:05:42 +0100

    m

--- MHA/settings.py
+++ MHA/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1,testserver,.railway.app,.up.railway.app',
+    default='localhost,127.0.0.1,testserver,.railway.app,.up.railway.app','www.magichoopsacadmy.com','magichoopsacadmy.com',,
     cast=Csv()
 )
 
